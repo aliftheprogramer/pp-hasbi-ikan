85a2ad4c39f7f13cf0fc343ff2e8339c7211284f
aliftheprogramer <alifarya679@gmail.com>
2025-12-20 01:22:43 +0700

    feat: Add registration success state and UI flow to display a success message and navigate to the login screen.

--- lib/core/constant/api_urls.dart
+++ lib/core/constant/api_urls.dart
@@ -1,6 +1,5 @@
 class ApiUrls {
-  static const String baseUrl =
-      "https://beetle-sincere-obviously.ngrok-free.app/api";
+  static const String baseUrl = "https://pp-hasbi-backend-i9rs.vercel.app/api";
   static const String login = "$baseUrl/auth/login";
   static const String register = "$baseUrl/auth/register";
   static const String fish = "$baseUrl/fish";
